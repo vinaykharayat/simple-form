5ca0566e003748629fff013e6f9560527468515f
testing <testing@vinaykharayat>
2021-01-15 18:54:31 +0530

    fix 1

--- php/welcome.php
+++ php/welcome.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        echo '<h1>Welcome'. filter_input(INPUT_POST, 'name');
-        ?>
-    </body>
-</html>
+<?php
+
+// put your code here
+echo '<h1>Welcome' . filter_input(INPUT_POST, 'name');
+
